2e0947a0de504ae99b9f45b2f1e103f0be473766
Aditya Aman <adityaaman22948@gmail.com>
2025-01-23 22:13:22 +0530

    fix

--- server/index.js
+++ server/index.js
@@ -14,6 +14,7 @@ app.use(
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Include OPTIONS for preflight
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true, // Allow credentials (e.g., cookies, headers)
+    preflightContinue: true,
   })
 );
 
