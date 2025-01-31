946b02c36397a0586f4639fc842b677228285691
Aditya Aman <adityaaman22948@gmail.com>
2025-02-01 00:16:49 +0530

    bug fixes

--- public/src/pages/Chat.jsx
+++ public/src/pages/Chat.jsx
@@ -14,50 +14,58 @@ export default function Chat() {
   const [contacts, setContacts] = useState([]);
   const [currentChat, setCurrentChat] = useState(undefined);
   const [currentUser, setCurrentUser] = useState(undefined);
-  useEffect(async () => {
-    if (!localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)) {
-      navigate("/login");
-    } else {
-      setCurrentUser(
-        await JSON.parse(
-          localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)
-        )
-      );
-    }
+
+  // Fetch current user from localStorage
+  useEffect(() => {
+    const fetchUser = async () => {
+      const localStorageKey = process.env.REACT_APP_LOCALHOST_KEY || "chat-user";
+      if (!localStorage.getItem(localStorageKey)) {
+        navigate("/login");
+      } else {
+        setCurrentUser(JSON.parse(localStorage.getItem(localStorageKey)));
+      }
+    };
+    fetchUser();
   }, []);
+
+  // Initialize Socket.IO connection
   useEffect(() => {
     if (currentUser) {
       socket.current = io(host);
       socket.current.emit("add-user", currentUser._id);
+
+      return () => {
+        socket.current.disconnect(); // Cleanup function
+      };
     }
   }, [currentUser]);
 
-  useEffect(async () => {
-    if (currentUser) {
-      if (currentUser.isAvatarImageSet) {
-        const data = await axios.get(`${allUsersRoute}/${currentUser._id}`);
-        setContacts(data.data);
-      } else {
-        navigate("/setAvatar");
+  // Fetch contacts
+  useEffect(() => {
+    const fetchContacts = async () => {
+      if (currentUser) {
+        if (currentUser.isAvatarImageSet) {
+          const data = await axios.get(`${allUsersRoute}/${currentUser._id}`);
+          setContacts(data.data);
+        } else {
+          navigate("/setAvatar");
+        }
       }
-    }
+    };
+    fetchContacts();
   }, [currentUser]);
+
   const handleChatChange = (chat) => {
     setCurrentChat(chat);
   };
+
   return (
-    <>
-      <Container>
-        <div className="container">
-          <Contacts contacts={contacts} changeChat={handleChatChange} />
-          {currentChat === undefined ? (
-            <Welcome />
-          ) : (
-            <ChatContainer currentChat={currentChat} socket={socket} />
-          )}
-        </div>
-      </Container>
-    </>
+    <Container>
+      <div className="container">
+        <Contacts contacts={contacts} changeChat={handleChatChange} />
+        {currentChat === undefined ? <Welcome /> : <ChatContainer currentChat={currentChat} socket={socket} />}
+      </div>
+    </Container>
   );
 }
 
@@ -69,11 +77,11 @@ const Container = styled.div`
   justify-content: center;
   gap: 1rem;
   align-items: center;
-  background-color:rgb(42, 50, 24);
+  background-color: rgb(42, 50, 24);
   .container {
     height: 85vh;
     width: 85vw;
-    background-color:rgba(88, 93, 57, 0.46);
+    background-color: rgba(88, 93, 57, 0.46);
     display: grid;
     grid-template-columns: 25% 75%;
     @media screen and (min-width: 720px) and (max-width: 1080px) {
