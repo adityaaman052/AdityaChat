1d327fb99193b3983c53b928b0b66c4635f5f73d
Aditya Aman <adityaaman22948@gmail.com>
2025-03-22 20:35:51 +0530

    / in login

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,6 +1,6 @@
 export const host = process.env.REACT_APP_API_HOST;
 
-export const loginRoute = `${host}/api/auth/login`;
+export const loginRoute = `${host}api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
 export const allUsersRoute = `${host}/api/auth/allusers`;
